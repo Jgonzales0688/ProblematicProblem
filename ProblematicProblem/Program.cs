3c0bf55300c4536f62bf1c66a8f58495e2c39bad
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-22 11:39:43 -0600

    updated logical error

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -46,7 +46,8 @@ namespace ProblematicProblem
             Console.Write("Would you like to add any activities before we generate one? yes/no: ");
             bool addToList = (Console.ReadLine().ToLower() == "yes" ? true : false);
 
-                Console.WriteLine();
+            Console.WriteLine();
+
             while (addToList)
             {
                 Console.Write("What would you like to add? ");
@@ -68,35 +69,36 @@ namespace ProblematicProblem
 
     while (cont)
     {
-        Console.Write("Connecting to the database");
-        for (int i = 0; i < 10; i++)
-        {
-            Console.Write(". ");
-            Thread.Sleep(500);
-        }
-                Console.WriteLine();
-                Console.Write("Choosing your random activity");
-        for (int i = 0; i < 9; i++)
-        {
-            Console.Write(". ");
-            Thread.Sleep(500);
-        }
+        //Console.Write("Connecting to the database");
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.Write(". ");
+        //    Thread.Sleep(500);
+        //}
+        //        Console.WriteLine();
+        //        Console.Write("Choosing your random activity");
+        //for (int i = 0; i < 9; i++)
+        //{
+        //    Console.Write(". ");
+        //    Thread.Sleep(500);
+        //}
         Console.WriteLine();
         int randomNumber = rng.Next(activities.Count);
-        string randomActivity = activities[randomNumber];
+                string randomActivity = "Wine Tasting"; //activities[randomNumber];
 
-        if (userAge > 21 && randomActivity == "Wine Tasting")
+        if (userAge < 21 && randomActivity == "Wine Tasting")
         {
             Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
             Console.WriteLine("Pick something else!");
             activities.Remove(randomActivity);
-            
-        }
-        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                    randomNumber = rng.Next(activities.Count);
+                    randomActivity = activities[randomNumber];
+                }
+        Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
         
         Console.WriteLine();
 
-        cont = (Console.ReadLine().ToLower() == "redo" ? true : false); ;
+        cont = (Console.ReadLine().ToLower() == "redo" ? true : false); 
         }
         }
     }
